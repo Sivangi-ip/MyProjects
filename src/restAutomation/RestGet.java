d362239d672d4558650cdc94e8861a1d46b6abd9
sivangi <sivangisharma.ip@gmail.com>
2016-10-25 16:57:31 -0700

    New Addition

---
+++ src/restAutomation/RestGet.java
@@ -0,0 +1,26 @@
+package restAutomation;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+public class RestGet {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		HttpClient client = HttpClientBuilder.create().build();
+		HttpGet request = new HttpGet("https://www.google.com/?q=java");
+		try {
+			HttpResponse response = client.execute(request);
+			System.out.println(response.getStatusLine());
+
+		} catch (Exception e) {
+			e.printStackTrace();
+
+		}
+
+	}
+}
